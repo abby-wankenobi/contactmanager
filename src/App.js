87067ba4934d94485977aa0459c580775292e222
Abigail Swarth <abigailswarth@gmail.com>
2018-10-15 16:38:34 -0400

    header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Contact from './components/Contact';
+import Header from './components/Header';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>New app</h1>
+        <Header />
+        <Contact />
       </div>
     );
   }
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>John Doe</h4>
+        <ul>
+          <li>Email: hello@gmail.com</li>
+          <li>Phone: 5555555555</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Contact;
