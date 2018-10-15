c9c1af838e3908237bbe0d9c0b7ed2ebd0ed5b74
Abigail Swarth <abigailswarth@gmail.com>
2018-10-15 16:55:03 -0400

    bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
